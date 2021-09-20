356c7972fd68a6d011832ca98b8427e452cd985c
moreauf@theobald-groupe.com <fm11071978>
2021-09-20 17:47:36 +0200

    Update OR

--- class/vehicule.class.php
+++ class/vehicule.class.php
@@ -938,7 +938,7 @@ class doliFleetVehicule extends SeedObject
         return $result;
     }
 
-    public function getLinkUrl($withpicto = 0, $moreparams = '', $fieldtodisplay = 'vin')
+    public function getLinkUrl($withpicto = 0, $moreparams = '', $fieldtodisplay = 'immatriculation,vin')
 	{
 		global $langs, $db;
 
@@ -971,9 +971,14 @@ class doliFleetVehicule extends SeedObject
 
 		if ($withpicto) $result.=($link.img_object($label, $picto, 'class="classfortooltip"').$linkend);
 		if ($withpicto && $withpicto != 2) $result.=' ';
-
-		$result.=$link.$this->{$fieldtodisplay}.$linkend;
-
+		$result.=$link;
+		$fields = explode(',',$fieldtodisplay);
+		$nb = count($fields);
+		$result .= $this->{$fields[0]};
+		for ($i = 1; $i <=$nb; $i++){
+			$result .= ' - ' . $this->{$fields[$i]};
+		}
+		$result .= $linkend;
 		return $result;
 	}
 
