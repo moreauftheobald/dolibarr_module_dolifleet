b84c290f718b33b1cabc6d6fb0dccb36dba7e5fe
Florian HENRY <florian.henry@open-concept.pro>
2024-02-07 15:00:59 +0100

    fix SQL

--- sql/llx_dolifleet_rental_proposaldet.sql
+++ sql/llx_dolifleet_rental_proposaldet.sql
@@ -1,9 +1,9 @@
 CREATE TABLE llx_dolifleet_rental_proposaldet (
-  rowid int(11) AUTO_INCREMENT PRIMARY KEY,
+  rowid integer AUTO_INCREMENT PRIMARY KEY,
   date_creation datetime DEFAULT NULL,
   tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  fk_vehicule int(11) DEFAULT 0,
-  fk_rental_proposal int(11) DEFAULT 0,
+  fk_vehicule integer DEFAULT 0,
+  fk_rental_proposal integer DEFAULT 0,
   total_ht double DEFAULT 0,
   description longtext DEFAULT NULL
 )ENGINE=innodb;
--- sql/llx_dolifleet_vehicule_link.sql
+++ sql/llx_dolifleet_vehicule_link.sql
@@ -1,11 +1,11 @@
 CREATE TABLE llx_dolifleet_vehicule_link (
-  rowid int(11) AUTO_INCREMENT PRIMARY KEY,
+  rowid integer AUTO_INCREMENT PRIMARY KEY,
   date_creation datetime DEFAULT NULL,
   tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   date_start datetime DEFAULT NULL,
   date_end datetime DEFAULT NULL,
-  fk_source int(11) DEFAULT 0,
-  fk_target int(11) DEFAULT 0,
-  fk_soc_vehicule_source int(11) NOT NULL DEFAULT 0,
-  fk_soc_vehicule_target int(11) NOT NULL DEFAULT 0
+  fk_source integer DEFAULT 0,
+  fk_target integer DEFAULT 0,
+  fk_soc_vehicule_source integer NOT NULL DEFAULT 0,
+  fk_soc_vehicule_target integer NOT NULL DEFAULT 0
 ) ENGINE=innodb;
