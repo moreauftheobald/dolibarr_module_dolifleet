bdf3340995fd96f114e2cb7cb277949e06f8d397
Theobald <f.moreau@theobald-groupe.com>
2026-02-23 15:49:04 +0100

    Class refactoring

--- class/vehiculeOperation.class.php
+++ class/vehiculeOperation.class.php
@@ -15,15 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if (!class_exists('SeedObject')) {
-	/**
-	 * Needed if $form->showLinkedObjectBlock() is call or for session timeout on our module page
-	 */
-	define('INC_FROM_DOLIBARR', true);
-	require_once dirname(__FILE__) . '/../config.php';
-}
+require_once DOL_DOCUMENT_ROOT.'/core/class/commonobject.class.php';
 
-class dolifleetVehiculeOperation extends SeedObject
+class dolifleetVehiculeOperation extends CommonObject
 {
 	/**
 	 * Draft status
@@ -47,43 +41,85 @@ class dolifleetVehiculeOperation extends SeedObject
 
 	/** @var array $TStatus Array of translate key for each const */
 	public static $TStatus = array(
-		self::STATUS_DRAFT => 'doliFleetOperationStatusShortDraft'
-	, self::STATUS_TOPLAN => 'doliFleetOperationStatusShortToPlan'
-	, self::STATUS_PLANNED => 'doliFleetOperationStatusShortPlanned'
-	, self::STATUS_DONE => 'doliFleetOperationStatusShortDone'
+		self::STATUS_DRAFT => 'doliFleetOperationStatusShortDraft',
+		self::STATUS_TOPLAN => 'doliFleetOperationStatusShortToPlan',
+		self::STATUS_PLANNED => 'doliFleetOperationStatusShortPlanned',
+		self::STATUS_DONE => 'doliFleetOperationStatusShortDone',
 	);
+
+	/** @var string $module Module name */
+	public $module = 'dolifleet';
 
 	/** @var string $table_element Table name in SQL */
 	public $table_element = 'dolifleet_vehicule_operation';
 
-	/** @var string $element Name of the element (tip for better integration in Dolibarr: this value should be the reflection of the class name with ucfirst() function) */
+	/** @var string $element Name of the element */
 	public $element = 'dolifleet_vehicule_operation';
+
+	/** @var int $isextrafieldmanaged Enable extrafields management */
+	public $isextrafieldmanaged = 0;
+
+	/** @var int $ismultientitymanaged 0=No test on entity, 1=Test with field entity, 2=Test with link by societe */
+	public $ismultientitymanaged = 0;
 
 	/** @var int $fk_vehicule Object link to vehicule */
 	public $fk_vehicule;
 
+	/** @var int $fk_product Product id */
 	public $fk_product;
 
+	/** @var int $status Status */
 	public $status;
 
+	/** @var double $km Km interval */
 	public $km;
 
+	/** @var int $delai_from_last_op Delay from last operation in months */
 	public $delai_from_last_op;
 
+	/** @var int|string $date_done Last operation date */
 	public $date_done;
 
+	/** @var int|string $date_due Due date */
 	public $date_due;
 
+	/** @var int $rang Sort rank */
 	public $rang;
 
+	/** @var double $km_done Last operation km */
 	public $km_done;
 
+	/** @var int|string $date_next Next operation date */
 	public $date_next;
+
+	/** @var double $km_next Next operation km */
 	public $km_next;
+
+	/** @var int $on_time On time flag */
 	public $on_time;
+
+	/** @var int $or_next Next operation order id */
 	public $or_next;
 
+	/** @var int|string $date_creation Creation date */
+	public $date_creation;
+
+	/** @var array Status label cache */
+	public $labelStatus = array();
+
+	/** @var array Status short label cache */
+	public $labelStatusShort = array();
+
 	public $fields = array(
+		'rowid' => array(
+			'type' => 'integer',
+			'label' => 'TechnicalID',
+			'enabled' => 1,
+			'visible' => 0,
+			'notnull' => 1,
+			'position' => 1,
+			'index' => 1,
+		),
 		'fk_vehicule' => array(
 			'type' => 'integer:Vehicule:dolifleet/class/vehicule.class.php',
 			'label' => 'Vehicule',
@@ -92,7 +128,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'position' => 10,
 			'index' => 1,
 		),
-
 		'fk_product' => array(
 			'type' => 'integer:Product:product/class/product.class.php',
 			'label' => 'VehiculeOperation',
@@ -101,7 +136,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'position' => 20,
 			'index' => 1,
 		),
-
 		'status' => array(
 			'type' => 'integer',
 			'label' => 'Status',
@@ -112,36 +146,32 @@ class dolifleetVehiculeOperation extends SeedObject
 			'index' => 1,
 			'position' => 30,
 			'arrayofkeyval' => array(
-				self::STATUS_DRAFT => 'doliFleetOperationStatusShortDraft'
-			, self::STATUS_TOPLAN => 'doliFleetOperationStatusShortToPlan'
-			, self::STATUS_PLANNED => 'doliFleetOperationStatusShortPlanned'
-			, self::STATUS_DONE => 'doliFleetOperationStatusShortDone'
+				0 => 'doliFleetOperationStatusShortDraft',
+				1 => 'doliFleetOperationStatusShortToPlan',
+				2 => 'doliFleetOperationStatusShortPlanned',
+				3 => 'doliFleetOperationStatusShortDone',
 			)
 		),
-
 		'rang' => array(
 			'type' => 'integer',
 			'visible' => 0,
 			'enabled' => 1,
-			'position' => 40
+			'position' => 40,
 		),
-
 		'km' => array(
 			'type' => 'double',
 			'visible' => 1,
 			'enabled' => 1,
-			'position' => 50
+			'position' => 50,
 		),
-
 		'delai_from_last_op' => array(
 			'type' => 'integer',
 			'label' => 'VehiculeOperationDelay',
 			'visible' => 1,
 			'enabled' => 1,
 			'position' => 60,
-			'comment' => 'delay from last operation in months'
+			'comment' => 'delay from last operation in months',
 		),
-
 		'date_done' => array(
 			'type' => 'date',
 			'label' => 'VehiculeOperationLastDateDone',
@@ -149,7 +179,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 70,
 		),
-
 		'km_done' => array(
 			'type' => 'double',
 			'label' => 'VehiculeOperationLastKmDone',
@@ -157,7 +186,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 80,
 		),
-
 		'date_next' => array(
 			'type' => 'date',
 			'label' => 'VehiculeOperationDateNext',
@@ -165,7 +193,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 85,
 		),
-
 		'date_due' => array(
 			'type' => 'date',
 			'label' => 'VehiculeOperationDateDue',
@@ -173,7 +200,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 90,
 		),
-
 		'km_next' => array(
 			'type' => 'double',
 			'label' => 'VehiculeOperationKmNext',
@@ -181,7 +207,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 95,
 		),
-
 		'on_time' => array(
 			'type' => 'integer',
 			'label' => 'VehiculeOperationOnTime',
@@ -189,7 +214,6 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 100,
 		),
-
 		'or_next' => array(
 			'type' => 'integer',
 			'label' => 'VehiculeOperationNextOR',
@@ -197,98 +221,46 @@ class dolifleetVehiculeOperation extends SeedObject
 			'enabled' => 1,
 			'position' => 105,
 			'default' => null,
-		)
-
+		),
+		'date_creation' => array(
+			'type' => 'datetime',
+			'label' => 'DateCreation',
+			'enabled' => 1,
+			'visible' => 0,
+			'notnull' => 0,
+			'position' => 500,
+		),
+		'tms' => array(
+			'type' => 'timestamp',
+			'label' => 'DateModification',
+			'enabled' => 1,
+			'visible' => 0,
+			'notnull' => 0,
+			'position' => 501,
+		),
 	);
 
 	/**
-	 * doliFleetVehiculeOperation constructor.
+	 * dolifleetVehiculeOperation constructor.
 	 * @param DoliDB $db Database connector
 	 */
 	public function __construct($db)
 	{
-		global $conf;
-
-		parent::__construct($db);
-
-		$this->init();
+		$this->db = $db;
 
 		$this->date_next = null;
 		$this->date_done = null;
 		$this->date_due = null;
-
-		$this->entity = $conf->entity;
 	}
 
 	/**
-	 * @param int $mode 0=Long label, 1=Short label, 2=Picto + Short label, 3=Picto, 4=Picto + Long label, 5=Short label + Picto, 6=Long label + Picto
-	 * @return string
+	 * Create object into database
+	 *
+	 * @param  User $user      User that creates
+	 * @param  int  $notrigger 0=launch triggers after, 1=disable triggers
+	 * @return int             Return integer <0 if KO, Id of created object if OK
 	 */
-	public function getLibStatut($mode = 0)
-	{
-		return self::LibStatut($this->status, $mode);
-	}
-
-	/**
-	 * @param int $status Status
-	 * @param int $mode 0=Long label, 1=Short label, 2=Picto + Short label, 3=Picto, 4=Picto + Long label, 5=Short label + Picto, 6=Long label + Picto
-	 * @return string
-	 */
-	public static function LibStatut($status, $mode)
-	{
-		global $langs;
-
-		$langs->load('dolifleet@dolifleet');
-		$res = '';
-
-		if ($status == self::STATUS_DRAFT) {
-			$statusType = 'status0';
-			$statusLabel = $langs->trans('doliFleetOperationStatusDraft');
-			$statusLabelShort = $langs->trans('doliFleetOperationStatusShortDraft');
-		} elseif ($status == self::STATUS_TOPLAN) {
-			$statusType = 'status6';
-			$statusLabel = $langs->trans('doliFleetOperationStatusToPlan');
-			$statusLabelShort = $langs->trans('doliFleetOperationStatusShortToPlan');
-		} elseif ($status == self::STATUS_PLANNED) {
-			$statusType = 'status1';
-			$statusLabel = $langs->trans('doliFleetOperationStatusPlanned');
-			$statusLabelShort = $langs->trans('doliFleetOperationStatusShortPlanned');
-		} elseif ($status == self::STATUS_DONE) {
-			$statusType = 'status4';
-			$statusLabel = $langs->trans('doliFleetOperationStatusDone');
-			$statusLabelShort = $langs->trans('doliFleetOperationStatusShortDone');
-		}
-
-		if (function_exists('dolGetStatus')) {
-			$res = dolGetStatus($statusLabel, $statusLabelShort, '', $statusType, $mode);
-		} else {
-			if ($mode == 0) $res = $statusLabel;
-			elseif ($mode == 1) $res = $statusLabelShort;
-			elseif ($mode == 2) $res = img_picto($statusLabel, $statusType) . $statusLabelShort;
-			elseif ($mode == 3) $res = img_picto($statusLabel, $statusType);
-			elseif ($mode == 4) $res = img_picto($statusLabel, $statusType) . $statusLabel;
-			elseif ($mode == 5) $res = $statusLabelShort . img_picto($statusLabel, $statusType);
-			elseif ($mode == 6) $res = $statusLabel . img_picto($statusLabel, $statusType);
-		}
-
-		return $res;
-	}
-
-	public function getName()
-	{
-		$ret = $this->fetch_product();
-		if ($ret > 0) return $this->product->getNomUrl(1) . ' ' . $this->product->label;
-		else return '';
-	}
-
-	public function getWebName()
-	{
-		$ret = $this->fetch_product();
-		if ($ret > 0) return '<b>' . $this->product->ref . '</b> - ' . $this->product->label;
-		else return '';
-	}
-
-	public function create(User &$user, $notrigger = false)
+	public function create(User $user, $notrigger = 0)
 	{
 		global $langs;
 
@@ -300,7 +272,7 @@ class dolifleetVehiculeOperation extends SeedObject
 			$this->errors[] = $langs->trans('ErrOperationNoProdId');
 		}
 
-		require_once DOL_DOCUMENT_ROOT . "/product/class/product.class.php";
+		require_once DOL_DOCUMENT_ROOT."/product/class/product.class.php";
 		$prod = new Product($this->db);
 		$ret = $prod->fetch($this->fk_product);
 		if ($ret <= 0) {
@@ -313,22 +285,141 @@ class dolifleetVehiculeOperation extends SeedObject
 
 		$this->calcNextOpe();
 
-		if (!empty($this->errors)) return -1;
+		if (!empty($this->errors)) {
+			return -1;
+		}
 
-		if (empty($this->id)) $this->rang = (int) $this->getMaxRank() + 1;
+		if (empty($this->id)) {
+			$this->rang = (int) $this->getMaxRank() + 1;
+		}
 
-		return parent::create($user, $notrigger);
+		return $this->createCommon($user, $notrigger);
 	}
 
-	public function update(User &$user, $notrigger = false)
+	/**
+	 * Load object in memory from the database
+	 *
+	 * @param  int    $id  Id object
+	 * @param  string $ref Ref
+	 * @return int         Return integer <0 if KO, 0 if not found, >0 if OK
+	 */
+	public function fetch($id, $ref = null)
+	{
+		return $this->fetchCommon($id, $ref);
+	}
+
+	/**
+	 * Update object into database
+	 *
+	 * @param  User $user      User that modifies
+	 * @param  int  $notrigger 0=launch triggers after, 1=disable triggers
+	 * @return int             Return integer <0 if KO, >0 if OK
+	 */
+	public function update(User $user, $notrigger = 0)
 	{
 		$this->calcNextOpe();
-		return parent::update($user, $notrigger);
+		return $this->updateCommon($user, $notrigger);
 	}
 
+	/**
+	 * Delete object in database
+	 *
+	 * @param  User $user      User that deletes
+	 * @param  int  $notrigger 0=launch triggers after, 1=disable triggers
+	 * @return int             Return integer <0 if KO, >0 if OK
+	 */
+	public function delete(User $user, $notrigger = 0)
+	{
+		return $this->deleteCommon($user, $notrigger);
+	}
+
+	/**
+	 * Return the status label
+	 *
+	 * @param  int $mode 0=Long label, 1=Short label, 2=Picto + Short label, 3=Picto, 4=Picto + Long label, 5=Short label + Picto, 6=Long label + Picto
+	 * @return string
+	 */
+	public function getLibStatut($mode = 0)
+	{
+		return $this->LibStatut($this->status, $mode);
+	}
+
+	/**
+	 * Return the status label for a given status
+	 *
+	 * @param  int $status Status
+	 * @param  int $mode   0=Long label, 1=Short label, 2=Picto + Short label, 3=Picto, 4=Picto + Long label, 5=Short label + Picto, 6=Long label + Picto
+	 * @return string
+	 */
+	public function LibStatut($status, $mode = 0)
+	{
+		// phpcs:enable
+		global $langs;
+
+		$langs->load('dolifleet@dolifleet');
+
+		if (empty($this->labelStatus) || empty($this->labelStatusShort)) {
+			$this->labelStatus[self::STATUS_DRAFT] = $langs->transnoentitiesnoconv('doliFleetOperationStatusDraft');
+			$this->labelStatus[self::STATUS_TOPLAN] = $langs->transnoentitiesnoconv('doliFleetOperationStatusToPlan');
+			$this->labelStatus[self::STATUS_PLANNED] = $langs->transnoentitiesnoconv('doliFleetOperationStatusPlanned');
+			$this->labelStatus[self::STATUS_DONE] = $langs->transnoentitiesnoconv('doliFleetOperationStatusDone');
+
+			$this->labelStatusShort[self::STATUS_DRAFT] = $langs->transnoentitiesnoconv('doliFleetOperationStatusShortDraft');
+			$this->labelStatusShort[self::STATUS_TOPLAN] = $langs->transnoentitiesnoconv('doliFleetOperationStatusShortToPlan');
+			$this->labelStatusShort[self::STATUS_PLANNED] = $langs->transnoentitiesnoconv('doliFleetOperationStatusShortPlanned');
+			$this->labelStatusShort[self::STATUS_DONE] = $langs->transnoentitiesnoconv('doliFleetOperationStatusShortDone');
+		}
+
+		$statusType = 'status0';
+		if ($status == self::STATUS_TOPLAN) {
+			$statusType = 'status6';
+		} elseif ($status == self::STATUS_PLANNED) {
+			$statusType = 'status1';
+		} elseif ($status == self::STATUS_DONE) {
+			$statusType = 'status4';
+		}
+
+		return dolGetStatus($this->labelStatus[$status], $this->labelStatusShort[$status], '', $statusType, $mode);
+	}
+
+	/**
+	 * Get product name with link
+	 *
+	 * @return string Product name with URL or empty string
+	 */
+	public function getName()
+	{
+		$ret = $this->fetch_product();
+		if ($ret > 0) {
+			return $this->product->getNomUrl(1).' '.$this->product->label;
+		}
+
+		return '';
+	}
+
+	/**
+	 * Get product name for web display
+	 *
+	 * @return string Product ref + label or empty string
+	 */
+	public function getWebName()
+	{
+		$ret = $this->fetch_product();
+		if ($ret > 0) {
+			return '<b>'.$this->product->ref.'</b> - '.$this->product->label;
+		}
+
+		return '';
+	}
+
+	/**
+	 * Get max rank for operations of this vehicule
+	 *
+	 * @return int Max rank value
+	 */
 	public function getMaxRank()
 	{
-		$sql = "SELECT MAX(rang) as maxrank FROM " . MAIN_DB_PREFIX . $this->table_element . " WHERE fk_vehicule = " . $this->fk_vehicule;
+		$sql = "SELECT MAX(rang) as maxrank FROM ".$this->db->prefix().$this->table_element." WHERE fk_vehicule = ".((int) $this->fk_vehicule);
 		$resql = $this->db->query($sql);
 		if ($resql) {
 			$obj = $this->db->fetch_object($resql);
@@ -338,40 +429,52 @@ class dolifleetVehiculeOperation extends SeedObject
 		return 0;
 	}
 
+	/**
+	 * Calculate next operation dates and km
+	 *
+	 * @return void
+	 */
 	public function calcNextOpe()
 	{
-		require_once DOL_DOCUMENT_ROOT . '/core/lib/date.lib.php';
+		require_once DOL_DOCUMENT_ROOT.'/core/lib/date.lib.php';
+
 		if (!empty($this->km)) {
 			$this->km_next = (int) $this->km_done + (int) $this->km;
 		} else {
-			$this->km_next=null;
+			$this->km_next = null;
 		}
 
 		if (empty($this->km) && !empty($this->delai_from_last_op)) {
 			$dt = dol_time_plus_duree($this->date_done, (int) $this->delai_from_last_op, 'm');
 			$this->date_due = dol_time_plus_duree($this->date_done, (int) $this->delai_from_last_op, 'm');
-			if ($dt>dol_now()) {
+			if ($dt > dol_now()) {
 				$this->date_next = dol_time_plus_duree($this->date_done, (int) $this->delai_from_last_op, 'm');
 			} else {
 				$this->date_next = dol_now();
 			}
 		}
 
-		if ($this->date_next<=dol_now() && empty($this->or_next)) {
-			$this->on_time=1;
+		if ($this->date_next <= dol_now() && empty($this->or_next)) {
+			$this->on_time = 1;
 		} else {
-			$this->on_time=0;
+			$this->on_time = 0;
 		}
 	}
 
+	/**
+	 * Check if operation needs update based on operation order lines
+	 *
+	 * @param  CommonObject $object Operation order object
+	 * @return bool                 True if operation needs update
+	 */
 	public function operationNeedUpdate($object)
 	{
-		//Si on a une ligne avec le même produit alors on met à jour l'opération
-		$sql = "SELECT rowid FROM " . MAIN_DB_PREFIX . $object->table_element_line . " WHERE fk_operation_order=" . $object->id . " AND fk_product=" . $this->fk_product;
+		$sql = "SELECT rowid FROM ".$this->db->prefix().$object->table_element_line." WHERE fk_operation_order=".((int) $object->id)." AND fk_product=".((int) $this->fk_product);
 		$resql = $this->db->query($sql);
 		if (!empty($resql) && $this->db->num_rows($resql) > 0) {
 			return true;
 		}
+
 		return false;
 	}
 }
