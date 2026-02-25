a40dcb74ddd0ccd04f8d37c695853c8d8e7413d9
Theobald <f.moreau@theobald-groupe.com>
2026-02-25 11:19:21 +0100

    Performance Optimisation

--- admin/vehicule_extrafields.php
+++ admin/vehicule_extrafields.php
@@ -92,6 +92,11 @@ $page_name = 'ExtraFields';
 
 llxHeader('', $langs->trans($page_name), $help_url);
 
+// Subheader
+$linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">'
+	. $langs->trans("BackToModuleList") . '</a>';
+print load_fiche_titre($langs->trans($page_name), $linkback);
+
 // Configuration header
 $head = dolifleetAdminPrepareHead();
 print dol_get_fiche_head(
--- lib/dolifleet.lib.php
+++ lib/dolifleet.lib.php
@@ -48,23 +48,7 @@ function dolifleetAdminPrepareHead()
 		$head[$h][0] = dol_buildpath("/dolifleet/admin/multicompany_sharing.php", 1);
 		$head[$h][1] = $langs->trans("multicompanySharing");
 		$head[$h][2] = 'multicompanySharing';
-		$h++;
 	}
-
-	$head[$h][0] = dol_buildpath("/dolifleet/admin/dolifleet_about.php", 1);
-	$head[$h][1] = $langs->trans("About");
-	$head[$h][2] = 'about';
-	$h++;
-
-	// Show more tabs from modules
-	// Entries must be declared in modules descriptor with line
-	//$this->tabs = array(
-	//	'entity:+tabname:Title:@dolifleet:/dolifleet/mypage.php?id=__ID__'
-	//); // to add new tab
-	//$this->tabs = array(
-	//	'entity:-tabname:Title:@dolifleet:/dolifleet/mypage.php?id=__ID__'
-	//); // to remove a tab
-	// complete_head_from_modules($conf, $langs, $object, $head, $h, 'dolifleet');
 
 	return $head;
 }
